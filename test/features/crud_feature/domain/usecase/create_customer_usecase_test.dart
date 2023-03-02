72dd738fa6c1c2278161550938546b73218366e3
m.kahtooei <m.kahtooei@gmail.com>
2023-03-02 09:38:14 +0330

    generate CustomerRepository Mock to perform test

---
+++ test/features/crud_feature/domain/usecase/create_customer_usecase_test.dart
@@ -0,0 +1,9 @@
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter_clean_architecture_tdd/features/crud_feature/domain/repository/customer_repository.dart';
+
+// Annotation to generate mock objects
+@GenerateMocks([CustomerRepository])
+
+// Main function to perform testing
+main() {}
