18d89ccbcebb63ef387e4ea8697f59375ac3c63e
m.kahtooei <m.kahtooei@gmail.com>
2023-03-02 12:13:50 +0330

    actual test for DeleteCustomerUseCase

--- lib/features/crud_feature/domain/usecase/delete_customer_usecase.dart
+++ lib/features/crud_feature/domain/usecase/delete_customer_usecase.dart
@@ -3,9 +3,13 @@ import 'package:flutter_clean_architecture_tdd/core/usecases/usecase.dart';
 import 'package:flutter_clean_architecture_tdd/features/crud_feature/domain/repository/customer_repository.dart';
 
 class DeleteCustomerUseCase extends UseCase<bool, int> {
+  // Declaring the `CustomerRepository` instance
   final CustomerRepository customerRepository;
+
+  // Constructor for `DeleteCustomerUseCase` class
   DeleteCustomerUseCase(this.customerRepository);
 
+// `execute` function returns a future that contains the `RequestStatus`
   @override
   Future<RequestStatus<bool>> execute(int customerID) {
     throw UnimplementedError();
--- test/features/crud_feature/domain/usecase/delete_customer_usecase_test.dart
+++ test/features/crud_feature/domain/usecase/delete_customer_usecase_test.dart
@@ -1,5 +1,7 @@
+import 'package:flutter_clean_architecture_tdd/core/resources/request_status.dart';
 import 'package:flutter_clean_architecture_tdd/features/crud_feature/domain/usecase/delete_customer_usecase.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'create_customer_usecase_test.mocks.dart';
 
 main() {
@@ -10,5 +12,38 @@ main() {
   setUp(() {
     //initiate DeleteCustomerUseCase with mock repository
     useCase = DeleteCustomerUseCase(customerRepository);
+  });
+
+  const int customerID = 1;
+  test('successful response for delete customer', () async {
+    // set up mock repository to return success result when deleteCustomer is called
+    when(customerRepository.deleteCustomer(customerID)).thenAnswer(
+        (realInvocation) => Future.value(SuccessRequest<bool>(true)));
+
+    // call execute method with customer ID on use case
+    var res = await useCase.execute(customerID);
+
+    // assert that the response is true
+    expect(res.response, true);
+    //verify that customerRepository.createNewCustomer called in this scope
+    verify(customerRepository.deleteCustomer(customerID));
+    // Only the above method should be called and nothing more.
+    verifyNoMoreInteractions(customerRepository);
+  });
+
+  test('failed response for delete customer', () async {
+    // set up mock repository to return failed result when deleteCustomer is called
+    when(customerRepository.deleteCustomer(customerID))
+        .thenAnswer((realInvocation) => Future.value(FailedRequest("Error")));
+
+    // call execute method with customer ID on use case
+    var res = await useCase.execute(customerID);
+
+    // assert that the error message is "Error"
+    expect(res.error, "Error");
+    //verify that customerRepository.createNewCustomer called in this scope
+    verify(customerRepository.deleteCustomer(customerID));
+    // Only the above method should be called and nothing more.
+    verifyNoMoreInteractions(customerRepository);
   });
 }
