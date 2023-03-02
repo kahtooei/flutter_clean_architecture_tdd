5015b14f25ef82bc61ff894bd13766318cf34e8c
m.kahtooei <m.kahtooei@gmail.com>
2023-03-02 10:09:46 +0330

    implement CreateCustomerUseCase class

--- lib/features/crud_feature/domain/usecase/create_customer_usecase.dart
+++ lib/features/crud_feature/domain/usecase/create_customer_usecase.dart
@@ -1,5 +1,8 @@
 // CreateCustomersUseCase class is a UseCase class that uses customerRepository
 // to create a new customer.
+import 'package:flutter_clean_architecture_tdd/core/params/customer_params.dart';
+import 'package:flutter_clean_architecture_tdd/core/resources/request_status.dart';
+import 'package:flutter_clean_architecture_tdd/features/crud_feature/domain/entities/customer_entity.dart';
 import 'package:flutter_clean_architecture_tdd/features/crud_feature/domain/repository/customer_repository.dart';
 
 class CreateCustomerUseCase {
@@ -8,4 +11,12 @@ class CreateCustomerUseCase {
 
   // Constructor that takes customerRepository as a parameter.
   CreateCustomerUseCase(this.customerRepository);
+
+  // execute method to create a new customer.
+  // It takes `CustomerParams` as a parameter and returns a `Future` of `RequestStatus<CustomerEntity>`.
+  Future<RequestStatus<CustomerEntity>> execute(
+      CustomerParams customerParams) async {
+    // Call `createNewCustomer` method on `customerRepository` to create a new customer.
+    return customerRepository.createNewCustomer(customerParams);
+  }
 }
