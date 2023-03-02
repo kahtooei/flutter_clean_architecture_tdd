33d7b1ab915e45fc31fe886f1411382e215d6acc
m.kahtooei <m.kahtooei@gmail.com>
2023-03-02 13:02:07 +0330

    implement EditCustomersUseCase class

--- lib/features/crud_feature/domain/usecase/edit_customer_usecase.dart
+++ lib/features/crud_feature/domain/usecase/edit_customer_usecase.dart
@@ -5,11 +5,18 @@ import 'package:flutter_clean_architecture_tdd/features/crud_feature/domain/enti
 import 'package:flutter_clean_architecture_tdd/features/crud_feature/domain/repository/customer_repository.dart';
 
 class EditCustomersUseCase extends UseCase<CustomerEntity, EditCustomerParams> {
+  // A reference to the customer repository is stored in a final instance variable.
   final CustomerRepository customerRepository;
+
+  // The constructor takes in the customer repository and stores it in the instance variable.
   EditCustomersUseCase(this.customerRepository);
+
+  // This method takes in the ID of the customer to edit and the customer params (the new data) and returns the result of calling `editCustomer` on the customer repository.
   @override
   Future<RequestStatus<CustomerEntity>> execute(
       EditCustomerParams editCustomerParams) {
-    throw UnimplementedError();
+    // Calling the `editCustomer` method from `customerRepository` to edit a customer
+    return customerRepository.editCustomer(
+        editCustomerParams.customerID, editCustomerParams.customerParams);
   }
 }
