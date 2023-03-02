f1e64598e70a1d7567726caeb2cbe87393b75924
m.kahtooei <m.kahtooei@gmail.com>
2023-03-02 09:51:21 +0330

    first test for CreateCustomerUseCase before define

--- test/features/crud_feature/domain/usecase/create_customer_usecase_test.dart
+++ test/features/crud_feature/domain/usecase/create_customer_usecase_test.dart
@@ -1,9 +1,18 @@
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:flutter_clean_architecture_tdd/features/crud_feature/domain/repository/customer_repository.dart';
+
+import 'create_customer_usecase_test.mocks.dart';
 
 // Annotation to generate mock objects
 @GenerateMocks([CustomerRepository])
 
 // Main function to perform testing
-main() {}
+main() {
+  MockCustomerRepository customerRepository = MockCustomerRepository();
+  late CreateCustomersUseCase useCase;
+  setUp(() {
+    useCase = CreateCustomersUseCase(customerRepository);
+  });
+}
