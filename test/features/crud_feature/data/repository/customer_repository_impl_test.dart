e6dedeadf3ec03fbb54e7648ca3a2c6e2edf4ae0
m.kahtooei <m.kahtooei@gmail.com>
2023-03-05 12:09:15 +0330

    add CustomerLocalDataSource Mock to perform test

---
+++ test/features/crud_feature/data/repository/customer_repository_impl_test.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_clean_architecture_tdd/features/crud_feature/data/data_source/local/customer_local_datasource.dart';
+import 'package:mockito/annotations.dart';
+
+// Annotation to generate mock objects
+@GenerateMocks([CustomerLocalDataSource])
+void main() {}
