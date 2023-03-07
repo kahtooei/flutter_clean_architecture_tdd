9219bf645c8acb6390126c5fcfe8433058e0dd84
m.kahtooei <m.kahtooei@gmail.com>
2023-03-07 10:48:07 +0330

    implement insertCustomer method in LocalDataSource

--- lib/features/crud_feature/data/data_source/local/customer_local_datasource.dart
+++ lib/features/crud_feature/data/data_source/local/customer_local_datasource.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_clean_architecture_tdd/core/params/customer_params.dart';
 import 'package:flutter_clean_architecture_tdd/features/crud_feature/data/models/customer_model.dart';
+import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 abstract class CustomerLocalDataSource {
@@ -17,16 +18,55 @@ class CustomerSqliteDB extends CustomerLocalDataSource {
   late Database db;
   Database? _db;
   CustomerSqliteDB({Database? database}) : _db = database;
+
+  late String _path;
+  final String _createCustomerTable = '''
+                  create table customer ( 
+                      id integer primary key autoincrement,
+                      firstName text not null,
+                      lastName text not null,
+                      dateOfBirth,
+                      phoneNumber text not null,
+                      email text UNIQUE not null,
+                      bankAccountNumber text not null,
+
+                      UNIQUE(firstName,lastName,dateOfBirth)
+                      )
+                  ''';
+
+  Future open({String dbname: "customer.db"}) async {
+    if (_db != null) {
+      db = _db!;
+    } else {
+      var dbpath = await getDatabasesPath();
+      _path = join(dbpath, dbname);
+      db = await openDatabase(_path, version: 1,
+          onCreate: (Database db, int version) async {
+        await db.execute(_createCustomerTable);
+      });
+    }
+  }
+
+  // Close the database connection
+  Future close() async {
+    db.close();
+  }
+
   @override
   Future<void> deleteCustomer(int customerID) {
     // TODO: implement deleteCustomer
     throw UnimplementedError();
   }
 
+// Insert data into the database
   @override
-  Future<CustomerModel> insertCustomer(CustomerParams customer) {
-    // TODO: implement insertCustomer
-    throw UnimplementedError();
+  Future<CustomerModel> insertCustomer(CustomerParams customer) async {
+    await open();
+    Map<String, dynamic> customerMap = customer.toMap();
+    int id = await db.insert("customer", customerMap);
+    customerMap['id'] = id;
+    await close();
+    return CustomerModel.fromMap(customerMap);
   }
 
   @override
