adae7d9ec13a3eb2633b439d07901da9ad1b5bc3
m.kahtooei <m.kahtooei@gmail.com>
2023-03-07 10:24:36 +0330

    first test sqlite localDataSource before define

---
+++ test/features/crud_feature/data/data_source/customer_local_datasource_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+
+Future<Database> getDB() async {
+  const String createCustomerTable = '''
+                  create table customer ( 
+                      id integer primary key autoincrement,
+                      firstName text not null,
+                      lastName text not null,
+                      dateOfBirth,
+                      phoneNumber text not null,
+                      email text UNIQUE not null,
+                      bankAccountNumber text not null,
+
+                      UNIQUE(firstName,lastName,dateOfBirth)
+                      )
+                  ''';
+  return await openDatabase(inMemoryDatabasePath, version: 1,
+      onCreate: (db, version) async {
+    await db.execute(createCustomerTable);
+  });
+}
+
+main() {
+  late Database db;
+  late CustomerSqliteDB localDataSource;
+  setUpAll(() async {
+    // Initialize FFI
+    sqfliteFfiInit();
+    // Change the default factory
+    databaseFactory = databaseFactoryFfi;
+    db = await getDB();
+    localDataSource = CustomerSqliteDB(database: db);
+  });
+}
