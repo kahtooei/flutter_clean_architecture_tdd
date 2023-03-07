b7fb3af223a0ef1a5164c2c4675c426cd8a450e1
m.kahtooei <m.kahtooei@gmail.com>
2023-03-07 10:40:08 +0330

    test for insertCustomer method in LocalDataSource

--- test/features/crud_feature/data/data_source/customer_local_datasource_test.dart
+++ test/features/crud_feature/data/data_source/customer_local_datasource_test.dart
@@ -1,4 +1,6 @@
+import 'package:flutter_clean_architecture_tdd/core/params/customer_params.dart';
 import 'package:flutter_clean_architecture_tdd/features/crud_feature/data/data_source/local/customer_local_datasource.dart';
+import 'package:flutter_clean_architecture_tdd/features/crud_feature/data/models/customer_model.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
@@ -33,5 +35,23 @@ main() {
     databaseFactory = databaseFactoryFfi;
     db = await getDB();
     localDataSource = CustomerSqliteDB(database: db);
+  });
+
+//customer parameters to test database
+  CustomerParams customerParams = CustomerParams(
+      firstName: "Mohammad",
+      lastName: "Kahtooei",
+      dateOfBirth: DateTime(1992, 09, 14),
+      phoneNumber: "00989179647448",
+      email: "m.kahtooei@gmail.com",
+      bankAccountNumber: "987456321");
+
+//test create new customer in database
+  test('Insert test', () async {
+    var result = await localDataSource.insertCustomer(customerParams);
+    // Check content
+    expect(result, isA<CustomerModel>());
+    expect(result.id, isA<int>());
+    expect(result.email, customerParams.email);
   });
 }
