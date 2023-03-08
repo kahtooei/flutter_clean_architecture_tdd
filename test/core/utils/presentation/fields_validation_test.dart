fa8295bdec9d9acb2d1950ca8aefcd9275732b03
m.kahtooei <m.kahtooei@gmail.com>
2023-03-08 13:51:08 +0330

    first test ParamValidation before define

---
+++ test/core/utils/presentation/fields_validation_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_clean_architecture_tdd/core/params/customer_params.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+main() {
+  late ParamsValidation paramsValidation;
+  late CustomerParams customerParams;
+  setUp(() {
+    customerParams = CustomerParams(
+        firstName: "Mohammad",
+        lastName: "Kahtooei",
+        bankAccountNumber: "10985478566",
+        dateOfBirth: DateTime.now(),
+        email: "m.kahtooei@gmail.com",
+        phoneNumber: "00989179647448");
+    paramsValidation = ParamsValidation(customerParams);
+  });
+}
