9026f6304d84093321104a442e06b04ab6e7242c
m.kahtooei <m.kahtooei@gmail.com>
2023-03-09 00:11:27 +0330

    test for EditCustomerEvent event

--- test/features/crud_feature/presentation/bloc/customer_bloc_test.dart
+++ test/features/crud_feature/presentation/bloc/customer_bloc_test.dart
@@ -1,5 +1,6 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_clean_architecture_tdd/core/params/customer_params.dart';
+import 'package:flutter_clean_architecture_tdd/core/params/edit_customer_params.dart';
 import 'package:flutter_clean_architecture_tdd/core/resources/request_status.dart';
 import 'package:flutter_clean_architecture_tdd/core/utils/presentation/fields_validation.dart';
 import 'package:flutter_clean_architecture_tdd/features/crud_feature/domain/entities/customer_entity.dart';
@@ -142,6 +143,56 @@ void main() {
       // await Future.delayed(const Duration(seconds: 5));
       verify(deleteCustomer.execute(1));
       verify(getCustomers.execute());
+    });
+  });
+
+//test edit customer event
+  group("edit customer bloc test", () {
+    setUp(() {
+      customerBloc = CustomerBloc(inputValidation, createCustomer, editCustomer,
+          deleteCustomer, getCustomers);
+      when(getCustomers.execute()).thenAnswer((_) =>
+          Future.value(SuccessRequest<List<CustomerEntity>>([customerEntity])));
+      when(editCustomer.execute(EditCustomerParams(
+              customerID: 1, customerParams: customerParams)))
+          .thenAnswer((_) =>
+              Future.value(SuccessRequest<CustomerEntity>(customerEntity)));
+      when(inputValidation.checkValidation(customerParams))
+          .thenReturn(ParamsValidationStatus(status: true));
+
+      when(inputValidation.checkValidation(customerInvalidParams)).thenReturn(
+          ParamsValidationStatus(status: false, error: "invalid params"));
+    });
+
+    blocTest(
+      "edit customer",
+      build: () => customerBloc,
+      act: (CustomerBloc bloc) => bloc.add(
+          EditCustomerEvent(customerID: 1, customerParams: customerParams)),
+      expect: () => [
+        CustomerState(customerStatus: CustomerLoadingStatus()),
+        CustomerState(customerStatus: CustomerCompletedStatus([customerEntity]))
+      ],
+    );
+
+    test("test calling methods", () async {
+      customerBloc.add(
+          EditCustomerEvent(customerID: 1, customerParams: customerParams));
+      await untilCalled(getCustomers.execute());
+      // await Future.delayed(const Duration(seconds: 5));
+      verify(createCustomer.execute(customerParams));
+      verify(getCustomers.execute());
+      verify(inputValidation.checkValidation(customerParams));
+    });
+
+    test("test error params validation", () async {
+      customerBloc.add(
+          EditCustomerEvent(customerID: 1, customerParams: customerParams));
+      await untilCalled(inputValidation.checkValidation(customerInvalidParams));
+      // await Future.delayed(const Duration(seconds: 2));
+      verify(inputValidation.checkValidation(customerInvalidParams));
+      expect(customerBloc.state.customerStatus,
+          FieldValidationErrorStatus("invalid params"));
     });
   });
 }
