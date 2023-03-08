e5493fab1d3822b465e0cb5eb7fd315d1f75da4c
m.kahtooei <m.kahtooei@gmail.com>
2023-03-08 16:42:35 +0330

    test for CreateNewCustomerEvent Event

--- test/features/crud_feature/presentation/bloc/customer_bloc_test.dart
+++ test/features/crud_feature/presentation/bloc/customer_bloc_test.dart
@@ -1,4 +1,8 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_clean_architecture_tdd/core/params/customer_params.dart';
+import 'package:flutter_clean_architecture_tdd/core/resources/request_status.dart';
 import 'package:flutter_clean_architecture_tdd/core/utils/presentation/fields_validation.dart';
+import 'package:flutter_clean_architecture_tdd/features/crud_feature/domain/entities/customer_entity.dart';
 import 'package:flutter_clean_architecture_tdd/features/crud_feature/domain/usecase/create_customer_usecase.dart';
 import 'package:flutter_clean_architecture_tdd/features/crud_feature/domain/usecase/delete_customer_usecase.dart';
 import 'package:flutter_clean_architecture_tdd/features/crud_feature/domain/usecase/edit_customer_usecase.dart';
@@ -7,6 +11,7 @@ import 'package:flutter_clean_architecture_tdd/features/crud_feature/presentatio
 import 'package:flutter_clean_architecture_tdd/features/crud_feature/presentation/bloc/customer_status.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
 import 'customer_bloc_test.mocks.dart';
 
@@ -30,8 +35,60 @@ void main() {
         deleteCustomer, getCustomers);
   });
 
+  final CustomerParams customerParams = CustomerParams(
+      firstName: "Mohammad",
+      lastName: "Kahtooei",
+      dateOfBirth: DateTime.now(),
+      phoneNumber: "+989179647448",
+      email: "m.kahtooei@gmail.com",
+      bankAccountNumber: "123654789");
+  final CustomerEntity customerEntity = CustomerEntity(
+      id: 1,
+      firstName: "Mohammad",
+      lastName: "Kahtooei",
+      dateOfBirth: DateTime.now(),
+      phoneNumber: "+989179647448",
+      email: "m.kahtooei@gmail.com",
+      bankAccountNumber: "123654789");
+
 //test initial state
   test("initial state is Empty", () {
     expect(customerBloc.state.customerStatus, EmptyCustomerStatus());
+  });
+
+  //test create new customer event
+  group("create new customer bloc test1", () {
+    setUp(() {
+      customerBloc = CustomerBloc(inputValidation, createCustomer, editCustomer,
+          deleteCustomer, getCustomers);
+      when(getCustomers.execute()).thenAnswer((_) =>
+          Future.value(SuccessRequest<List<CustomerEntity>>([customerEntity])));
+
+      when(createCustomer.execute(customerParams)).thenAnswer(
+          (_) => Future.value(SuccessRequest<CustomerEntity>(customerEntity)));
+
+      when(inputValidation.isValid)
+          .thenReturn(ParamsValidationStatus(status: true));
+    });
+    blocTest(
+      "create new customer",
+      build: () => customerBloc,
+      act: (CustomerBloc bloc) => bloc.add(
+        CreateNewCustomerEvent(customerParams: customerParams),
+      ),
+      expect: () => [
+        CustomerState(customerStatus: CustomerLoadingStatus()),
+        CustomerState(customerStatus: CustomerCompletedStatus([customerEntity]))
+      ],
+    );
+
+    test("test calling methods", () async {
+      customerBloc.add(CreateNewCustomerEvent(customerParams: customerParams));
+      await untilCalled(getCustomers.execute());
+      // await Future.delayed(const Duration(seconds: 5));
+      verify(createCustomer.execute(customerParams));
+      verify(getCustomers.execute());
+      verify(inputValidation.isValid);
+    });
   });
 }
