948ca52733df44b1474f3af181bcd8bca27e14df
m.kahtooei <m.kahtooei@gmail.com>
2023-03-09 10:13:10 +0330

    provide bloc for UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_clean_architecture_tdd/features/crud_feature/presentation/bloc/customer_bloc.dart';
 import 'package:flutter_clean_architecture_tdd/locator.dart';
 
 void main() async {
   await setUp();
-  runApp(const MyApp());
+  runApp(MultiBlocProvider(
+      providers: [
+        BlocProvider<CustomerBloc>(create: (_) => getIt<CustomerBloc>()),
+      ],
+      child: MaterialApp(
+        title: 'CRUD Code Test',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyApp(),
+      )));
 }
 
 class MyApp extends StatelessWidget {
